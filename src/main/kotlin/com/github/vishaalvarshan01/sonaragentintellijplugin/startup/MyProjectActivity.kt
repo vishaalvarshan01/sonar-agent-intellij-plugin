556b4895f60cf7c8ad2f22a62aabd998d5250896
GitHub Action <action@github.com>
2025-05-09 14:04:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/vishaalvarshan01/sonaragentintellijplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/vishaalvarshan01/sonaragentintellijplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.vishaalvarshan01.sonaragentintellijplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
